e3a92cc677e2838c76f0b4bcdaadbeecdb61e590
MH Murshed <arisdarif@gmail.com>
2021-08-04 21:08:25 +0600

    class average solution modified

--- structured-program/ClassAverage.c
+++ structured-program/ClassAverage.c
@@ -1,5 +1,7 @@
 // Fig 3.6: Class Average.c
 // Class average program with counter-controlled iteration
+
+// A class of ten students took a quiz. The grades (integers in the range 0 to 100) for this quiz are available to you. Determine the class average on the quiz
 #include <stdio.h>
 
 // function main begins program execution
